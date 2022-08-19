e21883e90f0aedb10866fba073a84a5819d63d77
Kforellana <keyorellana453@gmail.com>
2022-08-19 12:01:28 -0600

    se trabajó en index.php y en productos.html

--- contra.php
+++ contra.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-inclide '':
+include 'cs.php':
 if (isset($_POST['ingresar'])) {
     $usua=$conectar->real_escape_string($_POST['USUARIO']);
     $cont=$conectar->real_escape_string($_POST['CONTRASEÑA']);
@@ -9,6 +9,24 @@ if (isset($_POST['ingresar'])) {
         while ($row=$resultado->fetch_array()) {
             $usuar=$row['Usuario'];
             $contr=$row['Contraseña']
+
+        }
+        $resultado->close();
+    }
+    $conectar->close();
+    if (isset($usua) && isset($cont)) {
+        if ($usua==$usuar && $cont==$contr){
+            $_SESSION['LOGIN'] = TRUE;
+            $_SESSION['Usuario'] = $usua;
+            header("location:mesajeria.php")
+        }
+       
+        else{
+            echo '<script lenguage="javascript"'>;
+            echo 'alert("¡Datos erroneos!")
+            window.location = "contrasena";
+            </script>';
+
 
         }
     }
---
+++ index.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="diseños/button.css">
+  <title>Inicio</title>
+</head>
+
+<body>
+  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="images/banner3.jpg" width="100%" height="450px">
+      </div>
+      <div class="carousel-item">
+        <img src="images/banner1.jpg" width="100%" height="450px">
+      </div>
+      <div class="carousel-item">
+        <img src="images/banner2.jpg" width="100%" height="450px">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <table width="100%">
+    <tr>
+      <td colspan="4">
+        <section class="a1">
+          <form action="productos.html" method="post">
+            <input type="submit" value="Servicios">
+          </form>
+        </section>
+      </td>
+      <td colspan="4">
+        <section class="a2">
+          <form action="tiendas.html" method="post">
+            <input type="submit" value="Tiendas">
+          </form>
+        </section>
+      </td>
+      <td colspan="4">
+        <section class="a3">
+          <form action="" method="post">
+            <input type="submit" value="Contactenos">
+          </form>
+        </section>
+      </td>
+    </tr>
+  </table>
+  <table width="100%">
+    <tr>
+    <td colspan="6" rowspan="2"> <img src="" width="px" height="px"> </td>
+    <td colspan="3"> <img src="" width="px" height="px"> </td>
+    <td colspan="3"> <img src="" width="px" height="px"> </td>
+    </tr>
+    <tr>
+      <td colspan="6"> <img src="" width="px" height="px"> </td>
+    </tr>
+    <tr>
+      <h1></h1>
+      <h3>OFERTAS DEL MES</h3>
+      <div>
+        <img src="images/1.jpg" alt="">
+      </div>
+      <div>
+        <img src="images/2.jpg" alt="">
+      </div>
+      <div>
+        <img src="images/3.jpg" alt="">
+      </div>
+      <div>
+        <img src="images/4.jpg" alt="">
+      </div>
+      <div>
+        <img src="images/5.jpg" alt="">
+      </div>
+    </tr>
+  </table>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
