a0875d2efc1ec69dc1d7901ff35feae37cbe7b7a
Kforellana <keyorellana453@gmail.com>
2022-09-10 22:27:27 -0600

    se trabajo en las 12 paginas de produnctos

---
+++ contac.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contactenos</title>
+      <link rel="stylesheet" href="button.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<table width="100%">
+    <tr>
+      <td colspan="4">
+        <section class="a">
+          <form action="productos.html" method="post">
+            <input type="submit" value="Productos">
+          </form>
+        </section>
+      </td>
+      <td colspan="4">
+        <section class="b">
+          <form action="tdd.php" method="post">
+            <input type="submit" value="Tiendas">
+          </form>
+        </section>
+      </td>
+      <td colspan="4">
+        <section class="c">
+          <form action="contac.php" method="post">
+            <input type="submit" value="Contactenos">
+          </form>
+        </section>
+      </td>
+    </tr>
+    </table>
+
+        <!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>TIENDAS</title>
+  
+  <body>
+
+         
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit] {
+  background-color: greenyellow;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.container {
+  border-radius: 1px;
+  background-color: #f2f22;
+  padding: 20px;
+}
+</style>
+</head>
+
+<div id="wrapper" class="container">              
+       
+        <br><br>
+                    <center><h1>Contactenos</h1><hr></center>
+                    <form class="w3-container" action="guardar.php" method="post">
+        <p>
+            <label>Nombres</label>
+            <input class="w3-input" type="text" name="a">
+        </p>
+        <p>
+            <label>Teléfono/Celular</label>
+            <input class="w3-input" type="text" name="b">
+        </p>
+        <p>
+            <label>Correo Electrónico</label>
+            <input class="w3-input" type="text" name="c">
+        </p>
+   <label for="subject">Mensaje</label>
+    <textarea id="subject" name="d" placeholder="Escribe un comentario" style="height:200px"></textarea>
+      
+        <input type="submit" value="Enviar Mensaje" name="e"> </center>
+        </form>
+        </div>
+
+</body>
+</html>
+
+
+
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+</html>
---
+++ tdd.php
@@ -0,0 +1,194 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="d.css">
+    <link rel="stylesheet" href="button.css">
+    <title>Tiendas</title>
+</head>
+
+<body style="background: url(images/fondo-1.jpg); background-size: cover; background-repeat: no-repeat;">
+    <table width="100%">
+    <tr>
+      <td colspan="4">
+        <section class="a">
+          <form action="productos.html" method="post">
+            <input type="submit" value="Productos">
+          </form>
+        </section>
+      </td>
+      <td colspan="4">
+        <section class="b">
+          <form action="tdd.php" method="post">
+            <input type="submit" value="Tiendas">
+          </form>
+        </section>
+      </td>
+      <td colspan="4">
+        <section class="c">
+          <form action="contac.php" method="post">
+            <input type="submit" value="Contactenos">
+          </form>
+        </section>
+      </td>
+    </tr>
+    </table>
+    <div class="container">
+        <div class="row align-items-start">
+            <div class="col-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d7721.015205135156!2d-90.51994172552726!3d14.627103259142212!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d14.623236799999999!2d-90.5163323!4m5!1s0x8589a22335840051%3A0x58edf114b49bb790!2sLibrer%C3%ADa%20Y%20Papeler%C3%ADa%20Progreso%2C%205%20Av%2016-42%2C%20Cdad.%20de%20Guatemala!3m2!1d14.6323213!2d-90.5161511!5e0!3m2!1ses!2sgt!4v1661263868449!5m2!1ses!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"   
+            </div>
+            <div class="col-4">
+                <p>Tel: 2320 2727</p><br>
+                <p>Dirección: 5 Av 16-42</p><br>
+                <p>Correo: libreriaprogreso@gmail.com</p><br>
+                <p>Horario: Lunes a viernes, 8:00 - 18:00 Sabado a Domingo, 08:00 - 13:00</p><br>
+            </div>
+        </div><br>
+        <div class="row align-items-center">
+            <div class="col-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d30885.72796703753!2d-90.53083046312861!3d14.615247823921015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e3!4m5!1s0x8589a308ce55a30d%3A0xffea4e1ff9c59ff8!2sLibrer%C3%ADa%20Progreso%20Quinta%20Avenida%20Zona%201%2C%205%20Av%2016-42%20Zona%201%20Ciudad%20de%20Guatemala%2C%2001001!3m2!1d14.6323213!2d-90.516151!4m3!3m2!1d14.598144!2d-90.5117696!5e0!3m2!1ses!2sgt!4v1661537189301!5m2!1ses!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>  
+            </div>
+            <div class="col-4">
+                <p>Tel: 2320 2727</p><br>
+                <p>Dirección: 5 Av 16-42 Zona 1 Ciudad de Guatemala</p><br>
+                <p>Correo:libreriaprogreso@gmail.com </p><br>
+                <p>Horario:Lunes a viernes, 8:00 - 18:00 Sabado a Domingo, 08:00 - 13:00</p><br>
+            </div>
+        </div><br>
+        <div class="row align-items-start">
+            <div class="col-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d61769.886232802804!2d-90.57146178903649!3d14.620830236039755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d14.598144!2d-90.5117696!4m5!1s0x8589a35f328bc81f%3A0xdd957a362617461a!2sLibrer%C3%ADa%20Y%20Papeler%C3%ADa%20Progreso!3m2!1d14.641696999999999!2d-90.52210269999999!5e0!3m2!1ses!2sgt!4v1661537539917!5m2!1ses!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>  
+            </div>
+            <div class="col-4">
+                <p>Tel: 2320 2727 </p><br>
+                <p>Dirección: 9 Calle 8-31, Zona 1 Ciudad de Guatemala</p><br>
+                <p>Correo:libreriaprogreso@gmail.com </p><br>
+                <p>Horario: Lunes a viernes, 8:00 - 18:00 Sabado a Domingo, 08:00 - 13:00</p><br>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d61769.886232802804!2d-90.57146178903649!3d14.620830236039755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d14.598144!2d-90.5117696!4m5!1s0x8589a35f328bc81f%3A0xdd957a362617461a!2sLibrer%C3%ADa%20Y%20Papeler%C3%ADa%20Progreso!3m2!1d14.641696999999999!2d-90.52210269999999!5e0!3m2!1ses!2sgt!4v1661537539917!5m2!1ses!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 2320 2727</p><br>
+                <p>Dirección: 9 Calle 8-31, Zona 1 Ciudad de Guatemala</p><br>
+                <p>Correo:libreriaprogreso@gmail.com </p><br>
+                <p>Horario:Lunes a viernes, 8:00 - 18:00 Sabado a Domingo, 08:00 - 13:00 </p><br>
+            </div>
+        </div>
+        <div class="row align-items-start">
+            <div class="col-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d61769.886232802804!2d-90.57146178903649!3d14.620830236039755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d14.598144!2d-90.5117696!4m5!1s0x8589a35f328bc81f%3A0xdd957a362617461a!2sLibrer%C3%ADa%20Y%20Papeler%C3%ADa%20Progreso!3m2!1d14.641696999999999!2d-90.52210269999999!5e0!3m2!1ses!2sgt!4v1661537539917!5m2!1ses!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 2320 2727</p><br>
+                <p>Dirección: 9 Calle 8-31, Zona 1 Ciudad de Guatemala</p><br>
+                <p>Correo: libreriaprogreso@gmail.com</p><br>
+                <p>Horario:Lunes a viernes, 8:00 - 18:00 Sabado a Domingo, 08:00 - 13:00 </p><br>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d61769.886232802804!2d-90.57146178903649!3d14.620830236039755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d14.598144!2d-90.5117696!4m5!1s0x8589a35f328bc81f%3A0xdd957a362617461a!2sLibrer%C3%ADa%20Y%20Papeler%C3%ADa%20Progreso!3m2!1d14.641696999999999!2d-90.52210269999999!5e0!3m2!1ses!2sgt!4v1661537539917!5m2!1ses!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 2320 2727</p><br>
+                <p>Dirección: 9 Calle 8-31, Zona 1 Ciudad de Guatemala</p><br>
+                <p>Correo: libreriaprogreso@gmail.com</p><br>
+                <p>Horario: Lunes a viernes, 8:00 - 18:00 Sabado a Domingo, 08:00 - 13:00</p><br>
+            </div>
+        </div>
+        <div class="row align-items-start">
+            <div class="col-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d61769.886232802804!2d-90.57146178903649!3d14.620830236039755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d14.598144!2d-90.5117696!4m5!1s0x8589a35f328bc81f%3A0xdd957a362617461a!2sLibrer%C3%ADa%20Y%20Papeler%C3%ADa%20Progreso!3m2!1d14.641696999999999!2d-90.52210269999999!5e0!3m2!1ses!2sgt!4v1661537539917!5m2!1ses!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 2320 2727</p><br>
+                <p>Dirección: 9 Calle 8-31, Zona 1 Ciudad de Guatemala</p><br>
+                <p>Correo:libreriaprogreso@gmail.com </p><br>
+                <p>Horario: Lunes a viernes, 8:00 - 18:00 Sabado a Domingo, 08:00 - 13:00</p><br>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d61769.886232802804!2d-90.57146178903649!3d14.620830236039755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d14.598144!2d-90.5117696!4m5!1s0x8589a35f328bc81f%3A0xdd957a362617461a!2sLibrer%C3%ADa%20Y%20Papeler%C3%ADa%20Progreso!3m2!1d14.641696999999999!2d-90.52210269999999!5e0!3m2!1ses!2sgt!4v1661537539917!5m2!1ses!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel:2320 2727 </p><br>
+                <p>Dirección:9 Calle 8-31, Zona 1 Ciudad de Guatemala </p><br>
+                <p>Correo: libreriaprogreso@gmail.com</p><br>
+                <p>Horario: Lunes a viernes, 8:00 - 18:00 Sabado a Domingo, 08:00 - 13:00</p><br>
+            </div>
+        </div>
+        <div class="row align-items-start">
+            <div class="col-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d61769.886232802804!2d-90.57146178903649!3d14.620830236039755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d14.598144!2d-90.5117696!4m5!1s0x8589a35f328bc81f%3A0xdd957a362617461a!2sLibrer%C3%ADa%20Y%20Papeler%C3%ADa%20Progreso!3m2!1d14.641696999999999!2d-90.52210269999999!5e0!3m2!1ses!2sgt!4v1661537539917!5m2!1ses!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel:2320 2727 </p><br>
+                <p>Dirección: 9 Calle 8-31, Zona 1 Ciudad de Guatemala</p><br>
+                <p>Correo: libreriaprogreso@gmail.com</p><br>
+                <p>Horario: Lunes a viernes, 8:00 - 18:00 Sabado a Domingo, 08:00 - 13:00</p><br>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d61769.886232802804!2d-90.57146178903649!3d14.620830236039755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d14.598144!2d-90.5117696!4m5!1s0x8589a35f328bc81f%3A0xdd957a362617461a!2sLibrer%C3%ADa%20Y%20Papeler%C3%ADa%20Progreso!3m2!1d14.641696999999999!2d-90.52210269999999!5e0!3m2!1ses!2sgt!4v1661537539917!5m2!1ses!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 2320 2727</p><br>
+                <p>Dirección: 9 Calle 8-31, Zona 1 Ciudad de Guatemala</p><br>
+                <p>Correo: libreriaprogreso@gmail.com</p><br>
+                <p>Horario: Lunes a viernes, 8:00 - 18:00 Sabado a Domingo, 08:00 - 13:00</p><br>
+            </div>
+        </div>
+        <div class="row align-items-start">
+            <div class="col-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d61769.886232802804!2d-90.57146178903649!3d14.620830236039755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d14.598144!2d-90.5117696!4m5!1s0x8589a35f328bc81f%3A0xdd957a362617461a!2sLibrer%C3%ADa%20Y%20Papeler%C3%ADa%20Progreso!3m2!1d14.641696999999999!2d-90.52210269999999!5e0!3m2!1ses!2sgt!4v1661537539917!5m2!1ses!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 2320 2727</p><br>
+                <p>Dirección: 9 Calle 8-31, Zona 1 Ciudad de Guatemala</p><br>
+                <p>Correo:libreriaprogreso@gmail.com </p><br>
+                <p>Horario: Lunes a viernes, 8:00 - 18:00 Sabado a Domingo, 08:00 - 13:00</p><br>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-8">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d61769.886232802804!2d-90.57146178903649!3d14.620830236039755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d14.598144!2d-90.5117696!4m5!1s0x8589a35f328bc81f%3A0xdd957a362617461a!2sLibrer%C3%ADa%20Y%20Papeler%C3%ADa%20Progreso!3m2!1d14.641696999999999!2d-90.52210269999999!5e0!3m2!1ses!2sgt!4v1661537539917!5m2!1ses!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel:2320 2727 </p><br>
+                <p>Dirección: 9 Calle 8-31, Zona 1 Ciudad de Guatemala</p><br>
+                <p>Correo: libreriaprogreso@gmail.com</p><br>
+                <p>Horario:Lunes a viernes, 8:00 - 18:00 Sabado a Domingo, 08:00 - 13:00 </p><br>
+            </div>
+            <table align="center" width="120%">
+        <tr style="background-color: yellow;">
+            <td colspan="1" align="left">
+                <h6 style="color: withe;">PBX: XXXXX-XXXXX</h6>
+            </td>
+            <td colspan="1" align="right">
+                <h6 style="color: left;">Dirección: XXXXX-XXXXX-XXXX</h6>
+                <h6></h6>
+            </td>
+            <td colspan="4">
+                .
+            </td>
+            <td colspan="1" align="right">
+                <h6>Carné: 2022-40454</h6>
+            </td>
+            <td colspan="1">.</td>
+            <td colspan="2" align="right">
+                <h6>Nombre: Keily Fabiola Orellana Marroquín</h6>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
